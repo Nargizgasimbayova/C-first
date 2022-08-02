52a810c138feb5744f8f4af16c31c8646e539960
Nargiz Gasimbayova <you@example.com>
2022-08-02 17:31:04 +0400

    added task 2

---
+++ task 2/task 2/task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 1;
+            int number2 = 100;
+            int counter = 0;
+            for (int i = number1; i < number2; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine(counter);
+            }
+
+         }
+    }
+}
