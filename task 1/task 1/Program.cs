565315f5e9b343d72c4c15d4a308e9455fb50ad4
Nargiz Gasimbayova <you@example.com>
2022-08-02 16:52:26 +0400

    first class

---
+++ task 1/task 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int month =8;
+
+			switch (month)
+			{
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+				case 8:
+				case 10:
+				case 12:
+					Console.Write("Month have 31 days.");
+					break;
+				case 2:
+					Console.Write("Month have 28 or 29 days.");
+					break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					Console.Write("Month have 30 days.");
+					break;
+				default:
+					Console.Write("invalid month number.");
+					break;
+			}
+            #endregion
+        }
+    }
+}
